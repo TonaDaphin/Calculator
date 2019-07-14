f455b6c6b287725dffc7dea3941ec909332d9220
Daniel <tonadaphine77@gmail.com>
2019-07-14 15:10:21 +0200

    Initialized git

---
+++ script.js
@@ -0,0 +1,18 @@
+function addfunction(){
+    cal.view.value += "+";
+}
+function subfunction(){
+    cal.view.value += "-";
+}
+function mulfunction(){
+    cal.view.value +="*";
+}
+function divfunction(){
+    cal.view.value +="/";
+}
+function resfunction(){
+    document.getElementById("view").value += " ";
+}
+function erafunction(){
+    document.cal.view.value= document.cal.view.value.substr(0,document.cal.view.value.length-1)
+}
